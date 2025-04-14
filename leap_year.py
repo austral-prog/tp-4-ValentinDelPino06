a90cc959a3be9f74c7054e2f81299b93f3e51a9b
ValentinDelPino06 <vdelpino@mail.austral.edu.ar>
2025-04-14 15:32:00 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        print(f"{año} es un año bisiesto.")
+    else:
+        print(f"{año} no es un año bisiesto.")
